1211e4f8d44ddc6caa5e343fbaceefeafb538392
vv4t <watashidesu6969@gmail.com>
2026-01-24 01:19:29 +1100

    update

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 sudo pacman -S pulseaudio pulseaudio-alsa alsa-utils --noconfirm
 sudo pacman -S python python-pip base-devel nmap --noconfirm
-sudo pacman -S xorg xorg-xinit pulseaudio pulseaudio-alsa --noconfirm
+sudo pacman -S xorg xorg-xinit pulseaudio pulseaudio-alsa  brightnessctl --noconfirm
 sudo pacman -S bspwm sxhkd --noconfirm
 
 git clone https://aur.archlinux.org/yay.git
@@ -11,12 +11,13 @@ makepkg -si
 cd ..
 
 sudo pacman -S kitty firefox feh ibus ibus-anthy maim rofi --noconfirm
-sudo pacman -S adobe-source-han-serif-jp-fonts papirus-icon-theme gtk3 imagemagick nerd-fonts --noconfirm
+sudo pacman -S adobe-source-han-serif-jp-fonts papirus-icon-theme gtk3 imagemagick nerd-fonts noto-fonts ttf-font-awesome --noconfirm
 sudo pacman -S thunar tumbler gvfs thunar-volman
 sudo pacman -S lxappearance arc-gtk-theme
 
 yay polybar
 yay ttf-iosevka
+yay ttf-icomoon-feather-git
 
 sudo pacman -S python-pywal --noconfirm
 
@@ -26,7 +27,7 @@ cp -r rofi ~/.config
 cp -r sxhkd ~/.config
 cp -r bspwm ~/.config
 cp -r polybar ~/.config
-cp -r "gtk-3.0" "~/.config"
+cp -r "gtk-3.0" ~/.config
 cp -r wal ~/.config
 cp -r nvim ~/.config
 cp -r kitty ~/.config
